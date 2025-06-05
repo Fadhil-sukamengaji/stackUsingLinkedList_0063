cc4b76dffa3d0a42676788f507e8e07d1eac4335
FadhilM <mudzakifadhil@gmail.com>
2025-06-05 17:28:00 +0700

    class node

--- stack.cpp
+++ stack.cpp
@@ -3,3 +3,15 @@
 #include <iostream>
 using namespace std;
 
+//node class representing a single node in the linked list
+class Node
+{
+    public : 
+    int data;
+    Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }  
+};
